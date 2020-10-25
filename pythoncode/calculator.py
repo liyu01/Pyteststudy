4f9f3c36a4953be638253d506a8650e2f5ff6a69
liyu01 <1766535261@qq.com>
2020-10-25 11:50:06 +0800

    计算器pytest

---
+++ pythoncode/calculator.py
@@ -0,0 +1,15 @@
+# -+- coding: utf-8 -*_
+
+# 被测试代码，计算机（加减乘除）
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def sub(self, a, b):
+        return a - b
+
+    def mul(self, a, b):
+        return a / b
+
+    def chen(self, a, b):
+        return a * b
---
+++ testing/test_demo.py
@@ -0,0 +1,8 @@
+# _*_ coding: utf-8 _*_
+
+def func(x):
+    return x + 1
+
+
+def test_answer():
+    assert func(3) == 5
